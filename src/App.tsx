d93b1934b4563c9d481774da3999a332add26dd6
Abhinav Shukla <149319019+StarLord824@users.noreply.github.com>
2025-03-28 12:49:19 +0000

    fc

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center h-screen bg-gray-900 text-white border-4 rounded-3xl border-purple-400'>
+        <h1 className='font-bold text-4xl my-2'>Phantom</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
